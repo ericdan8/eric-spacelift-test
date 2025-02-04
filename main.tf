45ced3eb29f9c9696fab575bb8ae0b4dcc1aa3e4
Eric Dan <ericdan@asana.com>
2025-02-04 15:48:04 -0800

    add spacelift context

--- main.tf
+++ main.tf
@@ -6,6 +6,25 @@ provider "aws" {
   region              = "us-east-1"
 }
 
+provider spacelift {}
+
+resource "spacelift_context" "terraform_1_10_1 {
+  description = "context for using terraform 1.10.1"
+  name = "Terraform 1.10.1"
+  before_init = [
+    "wget --quiet https://releases.hashicorp.com/terraform/1.10.1/terraform_1.10.1_linux_amd64.zip",
+    "unzip terraform_1.10.1_linux_amd64.zip",
+    "export PATH=$PATH:~/",
+    "mv terraform ~/terraform"
+  ]
+}
+
+resource "spacelift_mounted_file" "terraform_workflow_config" {
+  context_id = spacelift_context.terraform_1_10_1.id
+  relative_path = "source/.spacelift/workflow.yml"
+  content = filebase64("${path.module}/workflow.yml"))
+}
+
 resource "aws_s3_bucket" "example" {
   bucket        = "eric-spacelift-test-bucket-2"
 }
